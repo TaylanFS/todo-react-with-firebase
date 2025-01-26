80de230c64d822534c11eb830029a25203fd53f7
Taylan <ferreirataylan123@gmail.com>
2025-01-26 19:18:54 -0300

    added routes to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import RoutesApp from "./routes";
+
 export default function App() {
 	return (
-		<div>
-			<h1> Hello world ! </h1>
-		</div>
+		<BrowserRouter>
+			<RoutesApp />
+		</BrowserRouter>
 	);
 }
